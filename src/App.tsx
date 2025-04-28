a054efdc253836ab9d5c6c1a08d062cf5c1d8441
Marlon Ramos <marlonrdacosta@gmail.com>
2025-04-28 19:13:13 -0300

    Criando Rotas da Aplicação

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+function App() {
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<>LOGIN</>}/>
+        <Route path="/cadastro" element={<>Cadastro</>}/>
+        <Route path="/home" element={<>Home</>}/>
+        <Route path="/leads" element={<>Leads</>}/>
+        <Route path="/perfil" element={<>Perfil</>}/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
